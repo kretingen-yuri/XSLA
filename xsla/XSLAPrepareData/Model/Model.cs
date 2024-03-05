84b2b3df3ca90cc6b449a29b472a0005922501aa
yuri.kretingen <yuri.kretingen.zerto.com>
2024-03-05 16:10:27 +0200

    saving changes

---
+++ xsla/XSLAPrepareData/Model/Model.cs
@@ -0,0 +1,30 @@
+namespace XSLAPrepareData.Model;
+
+public class Model
+{
+    public int Filler1 { get; set; }
+    public string Issue { get; set; }
+    public string ToPlatform { get; set; }
+    public string FromPlatform { get; set; }
+    public string CustomerName { get; set; }
+    public string AffectedVersion { get; set; }
+    public int IsEscalationTeam { get; set; }
+    public int CreatedMonth { get; set; }
+    public double DaysSinceRelease { get; set; }
+    public string Severity { get; set; }
+    public int Priority { get; set; }
+    public string Initiative { get; set; }
+    public string ZVMZCAOsType { get; set; }
+    public string Component { get; set; }
+    public double NumberOfWorkingDays { get; set; }
+    public int Sentimental { get; set; }
+    public string UserImpact { get; set; }
+    public string WhatIsNeeded { get; set; }
+    public string Summary { get; set; }
+    public int ProblematicPlatform { get; set; }
+    public int CustomerName_int { get; set; }
+    public int AffectedVersion_int { get; set; }
+    public int Initiative_int { get; set; }
+    public int ZVMZCAOsType_int { get; set; }
+    public int Component_int { get; set; }
+}
--- xsla/XSLAPrepareData/Program.cs
+++ xsla/XSLAPrepareData/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics;
-using Newtonsoft.Json;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
 using NLog;
-using NLog.Fluent;
+using XSLAPrepareData.Model;
 using XSLARowDataFormatter.Entity;
 using XSLARowDataFormatter.Implementations;
 using XSLARowDataFormatter.Interfaces;
@@ -25,6 +27,7 @@ var mapEscalationsToJiraIssues = await client.RetrieveClosedEscalationsAsync();
 
 try
 {
+    Console.WriteLine("Build CSV file");
     logger.Info("Build CSV file");
     builder.Build(mapEscalationsToJiraIssues);
     logger.Info("Done");
@@ -36,6 +39,21 @@ try
     var rawData = await client.RetrieveEscalationAsync(escalation);
     builder.Build(new Dictionary<string, Issue> { { escalation, rawData } });
     Fixer(output, "escalation.csv", false);
+    logger.Info("Running AI");
+    using var reader = new StreamReader("escalation.csv");
+    
+    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        MissingFieldFound = null
+    };
+    
+    using var csv = new CsvReader(reader, config);
+    
+    csv.Read();
+    csv.ReadHeader();
+    csv.Read();
+    MLModel1.ModelInput record = csv.GetRecord<MLModel1.ModelInput>();
+    RunModel(record);
 }
 catch (Exception e)
 {
@@ -56,5 +74,66 @@ void Fixer(string inputFilename, string outputFilename, bool showPlot)
     using Process process = Process.Start(start);
     using StreamReader reader = process.StandardOutput;
     string result = reader.ReadToEnd();
-    logger.Info(result);
+    //logger.Info(result);
+}
+
+void RunModel(MLModel1.ModelInput realData)
+{
+    // Create single instance of sample data from first line of dataset for model input
+    MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
+    {
+        ToPlatform = realData.ToPlatform,
+        FromPlatform = realData.FromPlatform,
+        CreatedMonth = realData.CreatedMonth,
+        DaysSinceRelease = (float)realData.DaysSinceRelease,
+        Severity = realData.Severity,
+        Priority = realData.Priority,
+        Initiative = realData.Initiative,
+        ZVMZCAOsType = realData.ZVMZCAOsType,
+        Sentimental = realData.Sentimental,
+        CustomerName_int = realData.CustomerName_int,
+        AffectedVersion_int = realData.AffectedVersion_int,
+        Initiative_int = realData.Initiative_int,
+        Component_int = realData.Component_int,
+        // Install = true,
+        // Upgrade = false,
+        // Wa = true,
+        // ProblematicPlatform = true
+    };
+
+
+
+    Console.WriteLine("Using model to make single prediction -- Comparing actual NumberOfWorkingDays with predicted NumberOfWorkingDays from sample data...\n\n");
+
+
+    Console.WriteLine($"ToPlatform: {@"---"}");
+    Console.WriteLine($"FromPlatform: {@"Azure"}");
+    Console.WriteLine($"CreatedMonth: {2F}");
+    Console.WriteLine($"DaysSinceRelease: {4.00491F}");
+    Console.WriteLine($"Severity: {@"Normal"}");
+    Console.WriteLine($"Priority: {3F}");
+    Console.WriteLine($"Initiative: {@"ZVML"}");
+    Console.WriteLine($"ZVMZCAOsType: {@"Linux"}");
+    Console.WriteLine($"Component: {@"ZVM azure"}");
+    // Console.WriteLine($"NumberOfWorkingDays: {2.571286F}");
+    Console.WriteLine($"Sentimental: {0F}");
+    Console.WriteLine($"CustomerName_int: {1F}");
+    Console.WriteLine($"AffectedVersion_int: {1F}");
+    Console.WriteLine($"Initiative_int: {1F}");
+    Console.WriteLine($"ZVMZCAOsType_int: {1F}");
+    Console.WriteLine($"Component_int: {1F}");
+
+    try
+    {
+        var predictionResult = MLModel1.Predict(sampleData);
+        Console.WriteLine($"\n\nPredicted NumberOfWorkingDays: {predictionResult.Score}\n\n");
+
+        Console.WriteLine("=============== End of process, hit any key to finish ===============");
+        Console.ReadKey();
+    }
+    catch (Exception ex)
+    {
+        logger.Warn(ex, "Error occurred");
+        throw;
+    }
 }
